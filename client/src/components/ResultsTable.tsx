1945c06e7c4c12ce7f77aab1f9a151dc1cc64f3f
Nick Sutcliffe <nsutcliffe@gmail.com>
2025-07-13 22:54:02 +0100

    Make table responsive

--- client/src/components/ResultsTable.tsx
+++ client/src/components/ResultsTable.tsx
@@ -8,43 +8,45 @@ interface ResultsTableProps {
 function ResultsTable({ articles }: ResultsTableProps) {
   return (
     articles.length > 0 && (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col" style={{ whiteSpace: "nowrap" }}>
-              #
-            </th>
-            <th scope="col" style={{ whiteSpace: "nowrap" }}>
-              Published Date
-            </th>
-            <th scope="col">Title</th>
-            <th scope="col">Link to Source</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {articles.map((article, idx) => (
-            <tr key={idx + 1}>
-              <th scope="row">{idx + 1}</th>
-              <td>{article.publishedAt}</td>
-              <td>{article.title}</td>
-              <td>
-                <a
-                  href={article.url}
-                  className="truncate"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {article.url}
-                </a>
-              </td>
-              <td>
-                <SummariseButton article={article} />
-              </td>
+      <div className="table-responsive">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col" style={{ whiteSpace: "nowrap" }}>
+                #
+              </th>
+              <th scope="col" style={{ whiteSpace: "nowrap" }}>
+                Published Date
+              </th>
+              <th scope="col">Title</th>
+              <th scope="col">Link to Source</th>
+              <th scope="col"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {articles.map((article, idx) => (
+              <tr key={idx + 1}>
+                <th scope="row">{idx + 1}</th>
+                <td>{article.publishedAt}</td>
+                <td>{article.title}</td>
+                <td>
+                  <a
+                    href={article.url}
+                    className="truncate"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {article.url}
+                  </a>
+                </td>
+                <td>
+                  <SummariseButton article={article} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     )
   );
 }
